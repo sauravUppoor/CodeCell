6fdd20e5f0b54654c15a8c91b189635674396521
Saurav Uppoor <41644947+sauravUppoor@users.noreply.github.com>
2019-08-03 22:28:22 +0530

    Email feature upload

---
+++ email.py
@@ -0,0 +1,25 @@
+def check(email):
+	id = email.split('@')
+	newEmail = ""
+	id[0] = id[0].replace('.', '')
+	id[1] = id[1].replace('.', '')
+	if(id[0].find('#') != -1):
+		if(not ((id[0][0: id[0].find('#')] + id[1]) in unique)):
+			unique.append(id[0][0: id[0].find('#')] + id[1])
+			
+	else:
+
+		if(not ((id[0]+ id[1]) in unique)):
+			unique.append(id[0]+ id[1])
+			
+	
+
+ID = input()
+Email = ID.split(" ")
+unique = []
+
+for i in Email:
+	print(i)
+	check(i)
+print(len(unique))	
+	
