e50b727da24dc5c283b14c0336c02f6910941b33
Saurav Uppoor <41644947+sauravUppoor@users.noreply.github.com>
2019-08-12 11:58:39 +0530

    Update Marble.py

--- Marble.py
+++ Marble.py
@@ -1,31 +1,9 @@
-n = int(input())
-
-active = 0
-
-while(n != 0):
-    if(n % 3 == 0):
-         if(n > 1): 
-             n -= 2;
-         else: 
-         	n -= 1;
-         active = 1 - active;
-    elif((n - 1) % 3 == 0):
-         if(n > 2): 
-         	n -= 3;
-         else: 
-         	n -= 1;
-         active = 1 - active;
-    elif((n - 2) % 3 == 0):
-         if(n > 2): 
-         	n -= 3;
-         elif(n > 1):
-         	n -= 2;
-         else:
-         	n -= 1;
-         active = 1 - active;
-
-if(active == 0):
-	print(2);
-else:
-	print(1);
-         	
+n = [22, 45, 26, 75]
+#taking the test cases as input directly
+sum = 0
+for i in n:
+	if(i % 3 == 0):
+		sum += 2
+	elif((i - 1) % 3 == 0 or (i - 2) % 3 == 0):
+		sum += 1
+print(sum)   	
